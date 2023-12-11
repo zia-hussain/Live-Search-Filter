27dc3832e627112a4ed6f771f3fe44776f6021e8
zia-hussain <syedziashahgillani76@gmail.com>
2023-12-11 17:09:40 +0500

    Done !

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 // const users = document.getElementById('user-list');
-const user = document.querySelector('.user-list')
-const userName = document.getElementById('user')
+const users = document.querySelector(".user-list");
+const userName = document.getElementById("user");
+const userArr = [];
 
-const getUserData = async()=>{
+const getUserData = async () => {
+  try {
+    const res = await fetch("https://api.github.com/users");
+    const data = await res.json();
+    console.log(data);
 
-    try{
-const res = await fetch('https://api.github.com/users')
-const data =await res.json();
-console.log(data);
-
-data.map((user) => {
-    const li = document.createElement("li");
-
-    li.insertAdjacentHTML
-})
+    if (data) {
+      users.innerHTML = "";
     }
 
-    catch(e){
-console.log(e)
-    }
-}
+    data.map((user) => {
+      const li = document.createElement("li");
 
-getUserData()
+      userArr.push(li);
+
+      li.insertAdjacentHTML(
+        "afterbegin",
+        `
+    <div class="user-data">
+                    <img src=${user.avatar_url} alt=${user.avatar_url} srcset="">
+                    <div>
+                        <p>${user.login}</p>
+                        <a target="_blank" href=${user.html_url}>Github Profile</a>
+                    </div>
+                </div> `
+      );
+
+      users.appendChild(li);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+userName.addEventListener("input", (e) => {
+  const val = e.target.value;
+  console.log(val);
+
+  userArr.filter((currElem) => {
+    console.log(currElem);
+    currElem.innerText.toLowerCase().includes(val.toLowerCase())
+      ? currElem.classList.remove("hide")
+      : currElem.classList.add("hide");
+  });
+});
+
+getUserData();
